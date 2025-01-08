9b2faecb6651866b0a878d238af718ab1e338b21
Harshkhosla <harshkhosla9945@gmail.com>
2025-01-08 12:19:32 +0530

    adding the prisma to turbo repo

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,14 +1,27 @@
 import express from "express"
+import { PrismaClient } from '@repo/db';
 
-
+const client = new PrismaClient();
 const app = express()
 // app.cors()
 
+
+// read whay removed type module from there in pakate json what it dose
+
 app.get('/signup', async (req, res) => {
+
+    const data = await client.user.create({
+        data:{
+            email:"harsh",
+            password:"jdvhsdv",
+            username:"sample"
+        }
+    })
     res.send({
         message: "health chsdcvsdvvvsveck"
     })
 })
+
 
 
 
