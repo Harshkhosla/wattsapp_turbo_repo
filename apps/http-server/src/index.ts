ba3a846f721df3e9a6f320dbea7e97cf554742c0
Harsh Khosla <harshkhosla@Netomis-MacBook-Pro.local>
2025-01-29 21:44:42 +0530

    adding the login and sign up logic

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,28 +1,89 @@
 import express from "express"
 import { PrismaClient } from '@repo/db';
-// import { userSchema } from "@repo/common/src/types"
+import { CreateUserSchema, loginUserSchema } from "@repo/common/zod"
 import { JWT_SECRET } from "@repo/validators/config"
+import cors from 'cors';
+import jwt from "jsonwebtoken"
+import bycript from "bcrypt";
 
 const client = new PrismaClient();
 const app = express()
-// app.cors()
-
+app.use(cors())
+app.use(express.json())
 
 // read whay removsamnc ed type module from there in pakate json what it dose
 
-app.get('/signup', async (req, res) => {
+app.post('/signup', async (req, res) => {
 
-    console.log(JWT_SECRET);
-    const data = await client.user.create({
-        data: {
-            email: "harsh",
-            password: "jdvhsdv",
-            username: "sample"
+    const userData = req.body;
+    const Sheamdata = CreateUserSchema.safeParse(userData);
+
+    if (!Sheamdata.success) {
+        res.status(411).json({
+            message: Sheamdata.error
+        });
+        return;
+    }
+
+    const userExists = await client.user.findFirst({
+        where: {
+            email: userData.email
         }
     })
-    res.send({
-        message: "health chsdcvsdvvvsveck"
+
+    if (userExists) {
+        res.status(403).json({
+            messsage: "User already exists"
+        })
+    }
+
+    const Slat = 10;
+    const hashedPassword = await bycript.hash(userData.password, Slat);
+    const UserCreatedData = await client.user.create({
+        data: {
+            email: userData.email,
+            password: hashedPassword,
+            username: userData.username
+        }
     })
+    console.log(UserCreatedData);
+
+    res.send({
+        message: "User Created for you specally hapy to have you!",
+        user: { id: UserCreatedData.id, email: UserCreatedData.email }
+    })
+})
+
+app.post('/login', async (req, res) => {
+    const logindata = req.body;
+    const UserDataperfection = loginUserSchema.safeParse(logindata)
+
+    if (!UserDataperfection.success) {
+        res.status(411).json({
+            message: UserDataperfection.error
+        })
+        return;
+    }
+
+    const UserExist = await client.user.findFirst({
+        where: {
+            email: logindata.email
+        }
+    })
+    if (!UserExist) {
+        res.status(403).json({
+            message: "Your account is not been crrated you try diff One"
+        })
+        return;
+    }
+
+    const token = jwt.sign({ id: UserExist.id }, JWT_SECRET)
+
+    res.status(200).json({
+        message: "You have sucessuflly login In",
+        token
+    })
+    return;
 })
 
 
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,9 +1,14 @@
 import { z } from "zod";
 
 export const CreateUserSchema = z.object({
-  username: z.string().min(1),
+  username: z.string().min(3),
   email: z.string().email(),
-  password: z.string().min(6)
+  password: z.string().min(3)
 });
+
+export const loginUserSchema = z.object({
+  email:z.string().min(2),
+  password:z.string().min(2)
+})
 
 // export type UserSchema = z.infer<typeof userSchema>;
